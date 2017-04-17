9f49ec7e31931d4493633d0bb25486e79bf49995
Belve D. Marks IV <biv@localhost.localdomain>
2017-04-17 09:32:43 -0600

    map and array done

--- app/index.js
+++ app/index.js
@@ -10,13 +10,45 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        Hello Ben!
+        Hello {this.props.name}!
+      </div>
+    )
+  }
+}
+
+class FriendsContainer extends React.Component {
+  render() {
+    var name = 'Tyler McGinnis';
+    var friends = ['Ean Platter', 'Murphy Randall', 'Merrick Christensen'];
+    return (
+      <div>
+        <h3> Name: {name} </h3>
+        <ShowList names={friends} />
+      </div>
+    )
+  }
+}
+
+class ShowList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3> Friends </h3>
+        <ul>
+          {this.props.names.map(function(friend){
+            return <li> {friend} </li>;
+          })}
+        </ul>
       </div>
     )
   }
 }
 
 ReactDOM.render(
-  <App />,
+  <App name="World"/>,
   document.getElementById('app')
 );
+ReactDOM.render(
+    <FriendsContainer/>,
+    document.getElementById('friends')
+)
